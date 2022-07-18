7f4ba3643b8e08564bbe4b94a9f6eec896d8c259
David-Melamed <davidmelamed269@gmail.com>
2022-07-18 17:19:39 +0300

    fix database creation issue v0.1

--- SWAP/django_swap/SWAP/django_swap/mysql/mysql-post-commands.sql
+++ SWAP/django_swap/SWAP/django_swap/mysql/mysql-post-commands.sql
@@ -1,2 +1,8 @@
-CREATE DATABASE IF NOT EXISTS 'my_application';
+-- create database
+CREATE DATABASE IF NOT EXISTS my_application;
+
+-- create the user
+CREATE USER 'admin' IDENTIFIED BY 'password';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `my_application`.* TO 'admin'@'%';
+
 FLUSH PRIVILEGES;
