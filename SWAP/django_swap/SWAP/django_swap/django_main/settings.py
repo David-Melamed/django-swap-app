5e6c56861504156c66c3bad296c77d477ddce846
David-Melamed <davidmelamed269@gmail.com>
2024-06-03 12:01:28 +0300

    remove unnecessary comma

--- SWAP/django_swap/SWAP/django_swap/django_main/settings.py
+++ SWAP/django_swap/SWAP/django_swap/django_main/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'PORT': os.environ.get('DB_PORT')
         # 'OPTIONS': {
         #     'sql_mode': 'traditional',
         # }
